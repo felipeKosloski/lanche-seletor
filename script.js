38b90c5ddcde870eb27219fd5373ba07f6f4e6fe
felipeKosloski <felipe.kosloski.tkd@gmail.com>
2024-03-24 10:58:47 -0300

    feat: melhorias de layout e acrescentando imagens

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cardapio = {
         "lanches": {
             "grifinoria": {
@@ -74,19 +74,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const generateButton = document.getElementById('generateButton');
     const randomPedidoElement = document.getElementById('randomPedido');
+    const randomTituloPedidoElement = document.getElementById('randomTituloPedido');
+    
     let isButtonDisabled = false;
 
-    generateButton.addEventListener('click', function() {
+    generateButton.addEventListener('click', function () {
         if (isButtonDisabled) return; // Evita cliques repetidos enquanto o temporizador está ativo
-        randomPedidoElement.innerHTML = "sonserina não... sonserina não";
         isButtonDisabled = true;
-
         generateButton.disabled = true; // Desabilita o botão para evitar cliques repetidos
-        
+        generateButton.classList.add("disabled");
+        randomTituloPedidoElement.innerHTML = `
+            <p>sonserina não... sonserina não<p/>
+            <img id="chapéu-seletor" src="imagens/chapeu.png" alt="Chapéu Seletor" style="width: 100px; height: 100px;">`;
+
+
         const lanches = Object.keys(cardapio.lanches);
         const randomLancheKey = lanches[Math.floor(Math.random() * lanches.length)];
         const randomLanche = cardapio.lanches[randomLancheKey];
-        
+
         const adicionais = Object.keys(cardapio.adicionais);
         const randomAdicionalKey = adicionais[Math.floor(Math.random() * adicionais.length)];
         const randomAdicionalPrice = cardapio.adicionais[randomAdicionalKey];
@@ -94,18 +99,24 @@ document.addEventListener('DOMContentLoaded', function() {
         const bebidas = Object.keys(cardapio.bebidas);
         const randomBebidaKey = bebidas[Math.floor(Math.random() * bebidas.length)];
         const randomBebidaPrice = cardapio.bebidas[randomBebidaKey];
+        const imgPath = `imagens/${randomLancheKey}.png`;
 
         const totalPrice = randomLanche.preco + randomAdicionalPrice + randomBebidaPrice;
-        setTimeout(function() {
+        setTimeout(function () {
             isButtonDisabled = false;
             generateButton.disabled = false; // Habilita o botão após 5 segundos
-            randomPedidoElement.innerHTML = `
+            generateButton.classList.remove("disabled");
+            randomTituloPedidoElement.innerHTML = `
                 <h2 class="${randomLancheKey}">${randomLancheKey}</h2>
-                <p>Preço do Lanche: R$ ${randomLanche.preco.toFixed(2)}</p>
-                <p>Ingredientes: ${randomLanche.ingredientes.join(', ')}</p>
-                <p>Adicional: ${randomAdicionalKey} (+ R$ ${randomAdicionalPrice.toFixed(2)})</p>
-                <p>Bebida: ${randomBebidaKey} (+ R$ ${randomBebidaPrice.toFixed(2)})</p>
-                <p>Total: R$ ${totalPrice.toFixed(2)}</p>
+                <img src="${imgPath}" alt="${randomLancheKey}" style="width: 100px; height: 100px;">
+                `;
+
+            randomPedidoElement.innerHTML = `
+                <p><strong>Preço do Lanche:</strong> R$ ${randomLanche.preco.toFixed(2)}</p>
+                <p><strong>Ingredientes:</strong> ${randomLanche.ingredientes.join(', ')}</p>
+                <p><strong>Adicional:</strong> ${randomAdicionalKey} (+ R$ ${randomAdicionalPrice.toFixed(2)})</p>
+                <p><strong>Bebida:</strong> ${randomBebidaKey} (+ R$ ${randomBebidaPrice.toFixed(2)})</p>
+                <p><strong>Total:</strong> R$ ${totalPrice.toFixed(2)}</p>
             `;
         }, 5000); // Atraso de 5 segundos (5000 milissegundos)
     });
